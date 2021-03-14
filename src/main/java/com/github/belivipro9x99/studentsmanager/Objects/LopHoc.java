aa9ebebd3e6a88dad80ea4bc177ba660651cec92
AutoKeyboard <vutuandattb2002@gmail.com>
2021-03-14 18:14:55 +0700

    Thêm class LopHoc

---
+++ src/main/java/com/github/belivipro9x99/studentsmanager/Objects/LopHoc.java
@@ -0,0 +1,25 @@
+package com.github.belivipro9x99.studentsmanager.Objects;
+
+public class LopHoc {
+    String maLop;
+    String monHoc;
+    int soTinChi;
+    NgayThang thoiGian;
+    PhongHoc phongHoc;
+    SinhVien[] sinhVien;
+    GiangVien giangVien;
+    int trangThai;
+public String getTrangThai(){
+    return new String[]{
+        "Học",
+        "Học online",
+        "Nghỉ"
+    }[trangThai];
+}
+
+@Override
+public String toString(){
+    return String.format("LopHoc[maLop=%s]");
+}
+}
+
