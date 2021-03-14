e1a13dbb4b5c316dd055cd76f5ba08d67695f6bb
Belikhun <belivipro9x99@gmail.com>
2021-03-14 18:18:03 +0700

    Thêm classs Khoa

--- src/main/java/com/github/belivipro9x99/studentsmanager/Objects/Khoa.java
+++ src/main/java/com/github/belivipro9x99/studentsmanager/Objects/Khoa.java
@@ -4,4 +4,14 @@ public class Khoa {
 	public String ten = "Trường Đại Học Mở Hà Nội";
 	public String diaChi = "69 Định Công, Thanh Xuân, Hà Nội";
 	public NhanSu truongKhoa;
+	public NhanSu phoKhoa;
+
+	public GiangVien[] giangVien;
+	public SinhVien[] sinhVien;
+	public PhongHoc[] phongHoc;
+
+	@Override
+	public String toString() {
+		return String.format("Khoa[ten=%s]", ten);
+	}
 }
