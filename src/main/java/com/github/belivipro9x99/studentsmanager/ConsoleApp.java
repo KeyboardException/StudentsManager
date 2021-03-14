ded7be9264119b55a7e8f4a5dc1fce9fe9ecbf88
hoavochu <nccanh8a@gmail.com>
2021-03-14 23:01:39 +0700

    them fewfwf giảng viên

--- src/main/java/com/github/belivipro9x99/studentsmanager/ConsoleApp.java
+++ src/main/java/com/github/belivipro9x99/studentsmanager/ConsoleApp.java
@@ -2,6 +2,7 @@ package com.github.belivipro9x99.studentsmanager;
 
 import java.io.File;
 
+import com.github.belivipro9x99.studentsmanager.Objects.GiangVien;
 import com.github.belivipro9x99.studentsmanager.Objects.KhoaController;
 import com.github.belivipro9x99.studentsmanager.Objects.SinhVien;
 
@@ -13,9 +14,13 @@ public class ConsoleApp {
 		khoaController = new KhoaController(saveFile);
 
 		//khoaController.addSinhVien(new SinhVien("Vu Tuan Dat", "20A234524141"));
+		khoaController.addGiangVien(new GiangVien("Nguyễn Đắc Phương Thảo", "A212412353225"));
 
 		for (SinhVien sinhVien: khoaController.getSinhVienList())
 			System.out.println(sinhVien);
+
+		for (GiangVien giangVien: khoaController.getGiangVienList())
+			System.out.println(giangVien);
 
 		khoaController.save(saveFile);
 	}
