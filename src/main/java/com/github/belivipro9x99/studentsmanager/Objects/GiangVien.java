ef8f48aec684043da27fc197cdae5a8e6edb68ce
hoavochu <nccanh8a@gmail.com>
2021-03-14 18:16:59 +0700

    thêm classsssssss GiangVien

---
+++ src/main/java/com/github/belivipro9x99/studentsmanager/Objects/GiangVien.java
@@ -0,0 +1,38 @@
+package com.github.belivipro9x99.studentsmanager.Objects;
+
+import java.util.ArrayList;
+
+public class GiangVien extends NhanSu {
+    public String maGV;
+    public ArrayList<LopHoc> lophoc;
+    public int trinhDoHocVan;
+
+    public GiangVien(){
+        maGV = "";
+        trinhDoHocVan = 0;
+    }
+
+    public GiangVien( String maGV, int trinhDoHocVan ){
+        this.maGV = maGV;
+        this.trinhDoHocVan = trinhDoHocVan;
+    }
+    public String getmaGV(){
+        return maGV;
+    }
+    public String gettrinhDoHocVan(){
+        return new String[] {
+            "Thạc Sĩ",
+            "Tiến Sĩ",
+            "Phó Giáo Sư",
+            "Giáo SƯ",
+        }[trinhDoHocVan];
+    }
+     @Override
+    public String toString() {
+        return "GiangVien{" +
+                "maGV=" + maGV +
+                "trinhDoHocVan=" + trinhDoHocVan +
+                '}';
+    }
+}
+
