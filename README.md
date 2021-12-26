dc5b070b85e1c4b56dfc74a0561fe84fb68a0b1e
Belikhun <belivipro9x99@gmail.com>
2021-12-26 12:25:55 +0700

    🧶 add notice

--- README.md
+++ README.md
@@ -3,6 +3,17 @@
 
 [FILE BÁO CÁO](B%C3%81O%20C%C3%81O.docx)
 
+```
+Copyright (C) Team KeyboardOverflow - contact.kbexcp@gmail.com
+
+We publish and distribute this code in the hope that it will be useful
+for learning purpose, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+Anyone can refer to this source code and the document come alongside
+with it. DO NOT COPY AND PASTE this code, or the document into your own
+personal/group project.
+```
+
 #### Running
 
 Installing Dependencies
--- src/main/java/com/github/belivipro9x99/studentsmanager/App.java
+++ src/main/java/com/github/belivipro9x99/studentsmanager/App.java
@@ -1,3 +1,14 @@
+/**
+ * Copyright (C) Team KeyboardOverflow - contact.kbexcp@gmail.com
+ *
+ * We publish and distribute this code in the hope that it will be useful
+ * for learning purpose, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * Anyone can refer to this source code and the document come alongside
+ * with it. DO NOT COPY AND PASTE this code, or the document into your own
+ * personal/group project.
+ */
+
 package com.github.belivipro9x99.studentsmanager;
 
 import javafx.application.Application;
